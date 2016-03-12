dfa4031034eb770a89bd8adfcc77a9e0889a5e23
chenrt <1101925754@qq.com>
2016-03-12 23:29:11 +0800

    prepared

---
+++ codes/solveEquation.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+// to find out all positive solutions for equation like: ax + by = c
+void solve(int a, int b, int c) {
+	int cnt = 0;
+	for (int x = 1; x <= c/a; ++x)
+		for (int y = 1; y <= c/b; ++y)
+			if (a*x + b*y == c)
+				printf("[solu %d] x=%d, y=%d\n", ++cnt, x, y);
+
+	if (cnt == 0)
+		printf("No suitable solution for the equation %dx + %dy = %d\n", a, b, c);
+}
+
+
+
+int main() {
+	int a, b, c;
+	scanf("%d%d%d", &a, &b, &c);
+	solve(a, b, c);
+
+	return 0;
+}
